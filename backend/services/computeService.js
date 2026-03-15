cea24b29eae1fd0351bd3743b4ce51d18e765f1a
Arbitra <varzzgazz16@gmail.com>
2026-03-15 22:19:21 +0700

    Create computeService.js

---
+++ backend/services/computeService.js
@@ -0,0 +1,20 @@
+module.exports = {
+
+    listCompute: async () => {
+
+        return [
+            {
+                id:1,
+                gpu:"A100",
+                price:"2.5/hour"
+            },
+            {
+                id:2,
+                gpu:"RTX4090",
+                price:"1.2/hour"
+            }
+        ]
+
+    }
+
+}
